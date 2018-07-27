0be8f545d9d7ee69d43352f87e69d4ac410b7b8e
Loïc <loic.dumas@c-s.fr>
2018-07-27 20:19:42 +0200

    WIP

--- .travis/on_success.sh
+++ .travis/on_success.sh
@@ -1,4 +1,4 @@
-if [ -n "${GITHUB_sirius_doc_API_KEY}" ]; then
+if [ -n "${GITHUB_TOKEN}" ]; then
     cd "$TRAVIS_BUILD_DIR"
     docker run -v $TRAVIS_BUILD_DIR/sphinx:/sphinx ldumas/sirius_dockerfile:sirius /bin/sh -c "cd /sphinx; make html"
     cd $TRAVIS_BUILD_DIR/sphinx/build/
